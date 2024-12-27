4b4905908b77111533700b830ccdbeeed05b1283
Luka Collier <98172286+LUKACO59@users.noreply.github.com>
2024-12-27 22:59:03 +0000

    git push -dm.c

--- dm.c
+++ dm.c
@@ -6,6 +6,19 @@ int w(int *G, int n, int s, int t) {
     return G[n*s+t];
 }
 
+int poids_cycle(int *G, int *c ,int n){
+    assert(n>1);
+    int res = 0;
+    int b=c[0];
+    int a=c[0];
+    for(int i =1; i<n ; i++){
+        res=res + w(G,n,a,c[i]);
+        a=c[i];
+    }
+    res=res+w(G,n,a,b);
+    return res;
+}
+
 int main(){
     int G0[25] =
     {0, 1, 3, 5, 4,
@@ -15,5 +28,8 @@ int main(){
     4, 7, 10, 9, 0};
     int w51 = w(G0,5,3,4);
     printf("%d\n",w51);
+    int c[5] = {0, 2, 4, 3, 1};
+    int res= poids_cycle(G0,c,5);
+    printf("%d\n",res);
     return 0;
 }
